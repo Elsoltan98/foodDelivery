8b354ba2fcd21d384460f465c6aa749bb398832d
MACBOOK <ben10a2992@gmail.com>
2021-10-26 20:47:04 +0200

    Add categories to home screen

---
+++ src/global/Data.ts
@@ -0,0 +1,8 @@
+export const filterData = [
+  {name: 'Fast food', image: require('./../../assets/icons/icon1.png'), id: 1},
+  {name: 'Burgers', image: require('./../../assets/icons/icon2.png'), id: 2},
+  {name: 'Salad', image: require('./../../assets/icons/icon3.png'), id: 3},
+  {name: 'Desert', image: require('./../../assets/icons/icon4.png'), id: 4},
+  {name: 'Chinese', image: require('./../../assets/icons/icon5.png'), id: 5},
+  {name: 'Mexican', image: require('./../../assets/icons/icon6.png'), id: 6},
+];
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,196 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {useState} from 'react';
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import {View} from 'react-native';
 import HomeHeader from '../../components/HomeHeader';
 import {SIGNIN} from '../../global/RoutesName';
+import {colors} from '../../global/styles';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {filterData} from '../../global/Data';
 
 const Home = () => {
+  const [delivery, setDelivery] = useState(true);
+  const [selected, setSelected] = useState(1);
   return (
     <View>
       <HomeHeader title="Xpress Food" name="arrowleft" type={SIGNIN} />
-      <Text>Welcome to Home screen</Text>
+      <ScrollView>
+        {/* Top Buttons */}
+        <View style={styles.topButtonsContainer}>
+          <TouchableOpacity onPress={() => setDelivery(true)}>
+            <View
+              style={[
+                styles.topButton,
+                {
+                  backgroundColor: delivery ? colors.buttons : colors.grey4,
+                },
+              ]}>
+              <Text>Delivery</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setDelivery(false)}>
+            <View
+              style={[
+                styles.topButton,
+                {
+                  backgroundColor: delivery ? colors.grey4 : colors.buttons,
+                },
+              ]}>
+              <Text>Pick up</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* Address container */}
+        <View style={styles.addressContainer}>
+          <View style={styles.address}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <MaterialIcons
+                name="location-on"
+                color={colors.grey1}
+                size={21}
+              />
+              <Text style={{paddingLeft: 5}}>22 address</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: colors.grey5,
+                paddingVertical: 5,
+                paddingHorizontal: 10,
+                borderRadius: 15,
+              }}>
+              <MaterialCommunityIcons
+                name="clock"
+                color={colors.grey1}
+                size={21}
+              />
+              <Text style={{paddingLeft: 5}}>Now</Text>
+            </View>
+          </View>
+          <MaterialIcons name="tune" size={28} color={colors.grey1} />
+        </View>
+
+        {/* Categories */}
+        <View>
+          <View
+            style={{
+              backgroundColor: colors.grey4,
+              padding: 10,
+              paddingHorizontal: 20,
+            }}>
+            <Text
+              style={{color: colors.grey1, fontSize: 17, fontWeight: 'bold'}}>
+              Categories
+            </Text>
+          </View>
+          <FlatList
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={filterData}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                onPress={() => setSelected(item.id)}
+                style={[
+                  styles.imageContainer,
+                  {
+                    backgroundColor:
+                      selected === item.id ? colors.buttons : colors.grey5,
+                  },
+                ]}>
+                <View style={styles.imageBack}>
+                  <Image source={item.image} style={styles.image} />
+                </View>
+                <Text
+                  style={[
+                    styles.nameText,
+                    {color: selected === item.id ? '#fff' : colors.grey1},
+                  ]}>
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
+        <View
+          style={{
+            backgroundColor: colors.grey4,
+            padding: 10,
+            paddingHorizontal: 20,
+          }}>
+          <Text style={{color: colors.grey1, fontSize: 17, fontWeight: 'bold'}}>
+            Free delivery now
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  topButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 10,
+  },
+  topButton: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 20,
+  },
+  addressContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginVertical: 15,
+  },
+  address: {
+    backgroundColor: colors.grey4,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flex: 1,
+    marginRight: 20,
+    borderRadius: 25,
+  },
+  imageContainer: {
+    padding: 10,
+    margin: 10,
+    marginTop: 20,
+    borderRadius: 20,
+  },
+  imageBack: {
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    padding: 10,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  nameText: {
+    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 15,
+    fontWeight: '600',
+  },
+});
 
 export default Home;
