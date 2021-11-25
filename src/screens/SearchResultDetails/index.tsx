f68c9093506f80dfade6cfa9eecbfae4af2ea83e
MACBOOK <ben10a2992@gmail.com>
2021-11-25 14:33:15 +0200

    Add react native tab view to project

--- src/screens/SearchResultDetails/index.tsx
+++ src/screens/SearchResultDetails/index.tsx
@@ -1,13 +1,101 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import {Icon} from 'react-native-elements';
 import SearchDetailsHeader from '../../components/SearchDetailsHeader';
+import {colors} from '../../global/styles';
+import {TabView, TabBar} from 'react-native-tab-view';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const initialLayout = SCREEN_WIDTH;
 
 const SearchResultDetails = ({route}) => {
   const {item, id} = route.params;
-  console.log(route.params);
+  const [index, setIndex] = React.useState(0);
+  const [routes] = useState([
+    {key: 'first', title: 'MENU'},
+    {key: 'second', title: 'INFO'},
+    {key: 'third', title: 'REVIEWS'},
+    {key: 'fourth', title: 'GALLERY'},
+  ]);
+
+  const UpdateRoute = () => {
+    return <View />;
+  };
+
+  const renderTabBar = props => {
+    return (
+      <TabBar
+        {...props}
+        indicatorStyle={{backgroundColor: 'white'}}
+        style={{backgroundColor: colors.buttons}}
+        activeColor={'#fff'}
+      />
+    );
+  };
   return (
-    <View>
+    <View style={{flex: 1}}>
       <SearchDetailsHeader id={id} image={item.images} />
+      <Text
+        style={{
+          textAlign: 'center',
+          marginTop: 10,
+          color: 'green',
+          fontSize: 17,
+          fontWeight: '600',
+        }}>
+        Get {item.discount}% off on food total
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          marginVertical: 10,
+        }}>
+        <View>
+          <Text style={{fontSize: 19, fontWeight: '800', color: colors.grey1}}>
+            {item.resturantName}
+          </Text>
+          <Text style={{fontSize: 16, color: colors.grey2}}>
+            {item.foodType}
+          </Text>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Icon type="AntDesign" name="star" size={21} color={colors.grey3} />
+            <Text style={{fontSize: 16, color: colors.grey2, marginRight: 5}}>
+              {item.averageReview}
+            </Text>
+            <Icon
+              type="Entypo"
+              name="location-pin"
+              size={21}
+              color={colors.grey3}
+            />
+            <Text style={{fontSize: 16, color: colors.grey2}}>
+              {item.farAway} m away
+            </Text>
+          </View>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{marginRight: 10}}>
+            <Text>Collect</Text>
+            <Text>5</Text>
+            <Text>min</Text>
+          </View>
+          <View>
+            <Text>Delivery</Text>
+            <Text>15</Text>
+            <Text>min</Text>
+          </View>
+        </View>
+      </View>
+
+      <TabView
+        navigationState={{index, routes}}
+        renderTabBar={renderTabBar}
+        renderScene={UpdateRoute}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        tabBarPosition="top"
+      />
     </View>
   );
 };
